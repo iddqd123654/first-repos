159b3191acef0b2fc0c23782e434fc3cb033c48f
student <fasadar4@gmail.com>
2023-10-03 18:02:17 +0300

    changes

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ def getHealth():
     health = 100
     while True:
         a = r.randint(1,20)
-        b = r.randint(1, 20)
-        if a>b:
-            health-=20
-        if health <= 0:
-            print("Ваше здоровье закончилось")
-            break
+        #b = r.randint(1, 20)
+        #if a>b:
+            #health-=20
+        #if health <= 0:
+           # print("Ваше здоровье закончилось")
+            #break
 getHealth()
 
 def getHealth1(a,b):
